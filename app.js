b24b8eabc55e05c78b30bd9d624c97fe03794e93
unknown <orevaorior@gmail.com>
2023-05-22 02:18:40 +0100

    final initial setup

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
-
+require('dotenv').config();
+// error handler
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const connectDB = require('./connetDB/connect');
+ 
 const app = express();
 
 app.use(express.json())
@@ -8,16 +13,20 @@ app.use('/', (req, res)=>{
     res.send('oya jumole ya jo looorrr baby wa joo lor')
 })
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const port = 3000
 
 const start =async()=>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, ()=>{
             console.log(`listening at port ${port}...`)
         })
     } catch (error) {
-        console.log(err)
-    }
+        console.log(err) 
+    } 
 } 
 
-start()
+start();
